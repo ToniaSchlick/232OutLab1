fc48ee18078f787e71680eea886a556a05f46344
Tonia Schlick <Tonia@Tonias-MacBook-Pro.local>
2016-09-17 20:35:43 -0600

    first file

---
+++ Huffman_Codding/src/Node.java
@@ -0,0 +1,21 @@
+
+public class Node {
+
+	String value;
+	int count;
+	Node left;
+	Node right;
+	Node linker;
+	Node linkerBack;
+
+	Node(String value, int count) {
+
+		this.value = value;
+		this.count = count;
+		this.left = null;
+		this.right = null;
+		this.linker = null;
+		this.linkerBack = null;
+	}
+	
+}
