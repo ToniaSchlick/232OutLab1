4ada01c41a61a1327bbb4893398473073a818723
Tonia Schlick <Tonia@Tonias-MacBook-Pro.local>
2016-09-17 19:38:24 -0600

    adding first files to project

---
+++ src/Tuple.java
@@ -0,0 +1,16 @@
+
+public class Tuple {
+ private char letter;
+ private int charCount;
+ 
+ public Tuple(char character, int count){
+	 letter = character;
+	 charCount = count;
+ }
+ public char getLetter(){
+	 return letter;
+ }
+ public int getCount(){
+	 return charCount;
+ }
+}
