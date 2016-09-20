24cd2adfe4e35ca2da7e78d4bed487b4ab82ec58
Tonia Schlick <Tonia@Tonias-MacBook-Pro.local>
2016-09-20 11:48:39 -0600

    re-worked all classes

--- Huffman_Codding/src/Driver.java
+++ Huffman_Codding/src/Driver.java
@@ -1,46 +1,159 @@
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.PriorityQueue;
 
-public class Driver {
-
-	public static void main(String[] args) {
-		
-		// Get Message
+/*****************************************************************************
+* Main Function: 
+******************************************************************************/
+public class Driver{
+	static Map<Character, String> myKey = new HashMap<Character, String>();
+	static String key = ""; // Will be sent in as a parameter to the makeKey function
+	
+	public static void main(String[] args) throws IOException{		
+	/* Will be replaced by newMessage()
+	****************************************************************/
 		Scanner input = new Scanner(System.in);
 	    System.out.print("Input text message: ");
 	    String message = input.nextLine();
-		
 		// Convert the string to MessageChar array
-		char[] messageChar = message.toCharArray();
+	    char[] messageChar = message.toCharArray();
+	    // Takes messageChar [] and makes an ArrayList of Nodes
+		ArrayList<Node> myNodes = establishNodes(messageChar);		
+		// Takes an ArrayList of Nodes and makes a PriorityQueue of Nodes 
+		// that is ordered by the each Nodes count instance variable from 
+		// largest to smallest.
+		PriorityQueue<Node> sortedNodes = new PriorityQueue<Node> (myNodes);		
+		// Takes a Ordered Priority Queue and creates a Huffman structure Node
+		Node huffmanRoot = genHuffman(sortedNodes);		
+		// Takes a Node that is the root of a Huffman structure and and an empty
+		// string and creates a hash map that will be the key for compressing 
+		// and uncompressing a message
+		makeKey(huffmanRoot, key);
+	/*********************************************************************/
 		
-		ArrayList<Tuple> myList = makeArray(messageChar);
-		for (int i = 0; i < myList.size(); i++){
-			System.out.print(myList.get(i).getLetter());
-			System.out.print(myList.get(i).getCount());
-		}
+//		System.out.println("Enter the corisponding letter to the action you wish to proform, ");
+//		int choice = getChar();
+//		while(true){
+//			System.out.println(" c - compress message, u - uncompressed message, t - tree or n - new message: ");
+//			int choice = getChar();
+//			switch(choice){
+//			case 'c':
+//				printCompressedMessage();
+//				break;
+//			case 'u':
+//				printUnCompMessage;
+//				break;
+//			case 't':
+//				printHuffmanStruct;
+//				break;
+//			case 'n':
+//				newMessage();
+//				break;
+//			default:
+//				System.out.print("Invalid entry\n");
+//			
+//			}
+//		}
+}
+	////////////////////////////End of main///////////////////////////////////
+
+// /*****************************************************************************
+//  * Function getMessage: This Function takes in a message containing multiple 
+//  * lines of text. The message will be submitted upon the input of a # symbol
+//  * * Returns: series of strings.
+//  *****************************************************************************
+//  */ public void newMessage(){	    
+//		System.out.print("Enter message followed by a # symble ");
+//		
+//		/* code to get input */
+//		
+//		// Convert the string to MessageChar array
+//	    char[] messageChar = message.toCharArray();
+//	    
+//	    // Takes messageChar [] and makes an ArrayList of Nodes
+//		ArrayList<Node> myNodes = establishNodes(messageChar);
+//		
+//		// Takes an ArrayList of Nodes and makes a PriorityQueue of Nodes 
+//		// that is ordered by the each Nodes count instance variable from 
+//		// largest to smallest.
+//		PriorityQueue<Node> sortedNodes = new PriorityQueue<Node> (myNodes);
+//		
+//		// Takes a Ordered Priority Queue and creates a Huffman structure Node
+//		Node huffmanRoot = genHuffman(sortedNodes);
+//		
+//		// Takes a Node that is the root of a Huffman structure and and an empty
+//		// string and creates a hash map that will be the key for compressing 
+//		// and uncompressing a message
+//		makeKey(huffmanRoot, key);
+//	}
+  	///////////////////////End of Function newMessage////////////////////////////
+
+/*****************************************************************************
+ * 
+ *****************************************************************************
+ */	public static char getChar() throws IOException{
+		String s = getString();
+		return s.charAt(0);
 	}
-	
-	/*******************************************************************
-	 * makeArray - iterates through a char array and constructs a single 
-	 * tuple for each letter that has an occurrence in the char array. The 
-	 * tuple class has two instance variables, the letter and the number of
-	 * times the letter occurs in the char array. It will then add the 
-	 * tuple to an arrayList.
-	 * -Input Parameter: char array
-	 * -Returns: ArrayList
-	 * *****************************************************************
-	 */
-	
-	public static ArrayList<Tuple> makeArray(char [] messageChar){
-		ArrayList<Tuple> dataArray = new ArrayList<Tuple>();
-		Tuple myTuple;
+  	///////////////////////End of Function getChar////////////////////////////
+  
+/*****************************************************************************
+ * 
+ *****************************************************************************
+ */	public static String getString() throws IOException
+	{
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		String s = br.readLine();
+		return s;
+	}
+	///////////////////////End of Function getString////////////////////////////
+ 
+ 
+/***************************************************************************** 
+ * function makes the key to compress and uncompress the message. The key
+ * is made by recursively traversing the huffman structure. When the function 
+ * traverses down to a leaf of the structure it will create a key set in a hash
+ * map consisting of a the letter and the string of 0's and 1's . The string of 
+ * 0's and 1's was made during the traversal by adding a 0 for every left path 
+ * taken and a 1 for every right path).
+ *	* Requires Parameter: A Node that is the root tree, Empty String
+ *****************************************************************************
+ */	public static void makeKey(Node myNode, String key){	
+		if (myNode.getLeftChild() == null){
+			System.out.println("{" + myNode.getLetter() + ", " + key + "}"); // will print the Key
+			myKey.put(myNode.getLetter(), key);
+			return;
+		}
+		makeKey(myNode.getLeftChild(), key + "0");		
+		if (myNode.getRightChild() == null){
+			return;
+		}
+		makeKey(myNode.getRightChild(), key + "1");
+	}
+ 	///////////////////////End of Function makeKey////////////////////////////
+
+/*****************************************************************************
+ * Function establishNodes: This function will iterate through the given char 
+ * array finding every type of letter that occurs. Once a new letter type is 
+ * found the char array is iterated through again while keeping a running count 
+ * of every occurrence of that letter. The letter and count are then sent in a 
+ * parameters to a node constructor. The node is then added to an ArrayList. 
+ *	* Requires Parameter: char array 
+ *	* Returns: ArrayList of Nodes.
+ *****************************************************************************
+ */	public static ArrayList<Node> establishNodes(char [] messageChar){
+		ArrayList<Node> nodeArray = new ArrayList<Node>();
+		Node myNode;
 		for (int i = 0; i < messageChar.length; i++){
 			int count = 0;
 			int j = 0;
 			boolean found = false;			
-			for(j = 0; j < dataArray.size(); j++){
-				if (messageChar[i] == dataArray.get(j).getLetter()){
+			for(j = 0; j < nodeArray.size(); j++){
+				if (messageChar[i] == nodeArray.get(j).getLetter()){
 					found = true;
 				}
 			}			
@@ -50,10 +163,61 @@ public class Driver {
 						count ++;
 					}	
 				}
-				myTuple = new Tuple (messageChar[i], count);
-				dataArray.add(myTuple);
+				myNode = new Node (messageChar[i], count);
+				nodeArray.add(myNode);
 			}
 		}
-		return dataArray;
+		return nodeArray;
 	}
+ 	///////////////////End of Function establishNodes/////////////////////////
+	
+/*****************************************************************************
+ * Function genHuffman: This function removes nodes from the given PriorityQueue
+ * of nodes two at a time and sends them in to new node constructor. 
+ * These nodes will then become the left and right children of the new node 
+ * and the new nodes count will be the sum of the two given nodes count.
+ * This action will repeat until there are only two nodes remaining. The final
+ * two nodes will be sent to the node constructor just as the nodes before it
+ * but this new node will then be returned by the function.
+ * 	* Requires Parameter: PriorityQueue of Nodes
+ * 	* Returns: A single Node
+ *****************************************************************************
+ */	public static Node genHuffman(PriorityQueue<Node> sortedList){
+		while (sortedList.size() > 2){			
+			sortedList.add (new Node (sortedList.poll(), sortedList.poll()));
+		}
+		return new Node(sortedList.poll(), sortedList.poll());
+	}
+ 	///////////////////End of Function genHuffman/////////////////////////////	
+ 
+ /*****************************************************************************
+  * Function printCompressedMessage: This Function uses the hash map key to 
+  * make a string of 0's and 1's that represent each character in the given 
+  * message [] 
+  * * Requires Parameter: char []
+  * * Returns: char []
+  *****************************************************************************
+  */
+ 
+ 	////////////////End of Function printCompressedMessage/////////////////////
+ 
+ /*****************************************************************************
+  * Function printUnCompMessage: This Function uses the hash map key to 
+  * uncompress the string of 0's and 1's that represent each character back 
+  * to characters
+  * * Requires Parameter: String
+  * * Returns: char []
+  *****************************************************************************
+  */
+
+ 	////////////////End of Function printUnCompMessage/////////////////////////
+ 
+ /*****************************************************************************
+  * Function printHuffmanStruct: This Function prints the tree that is the 
+  * Huffman structure.
+  * * Requires Parameter: huffmanRoot Node
+  * * Returns: series of strings.
+  *****************************************************************************
+  */
+ 	/////////////////End of Function printHuffmanStruct////////////////////////
 }
--- Huffman_Codding/src/Node.java
+++ Huffman_Codding/src/Node.java
@@ -1,21 +1,44 @@
-
-public class Node {
-
-	String value;
-	int count;
-	Node left;
-	Node right;
-	Node linker;
-	Node linkerBack;
-
-	Node(String value, int count) {
-
-		this.value = value;
-		this.count = count;
-		this.left = null;
-		this.right = null;
-		this.linker = null;
-		this.linkerBack = null;
+class Node implements Comparable<Node>{
+	public char letter; // data item (key)
+	public int count; // data item
+	public Node leftChild; // this node's left child
+	public Node rightChild; // this node's right child
+	
+	public Node(char myLetter, int myCount){
+		letter = myLetter;
+		count = myCount;
+		leftChild = null;
+		rightChild = null;
 	}
 	
-}
+	public Node(Node left, Node right){
+		leftChild = left;
+		rightChild = right;
+		count = left.getCount() + right.getCount();
+	}
+	
+	public void displayNode(){	
+		System.out.print('{');
+		System.out.print(letter);
+		System.out.print(", ");
+		System.out.print(count);
+		System.out.print("} ");
+	}
+	public char getLetter(){
+		 return letter;
+	 }
+	 public int getCount(){
+		 return count;
+	 } 
+	 public Node getLeftChild(){
+		 return leftChild;
+	 }
+	 public Node getRightChild(){
+		 return rightChild;
+	 }
+	 
+	@Override
+	public int compareTo(Node o) { 
+		return this.count - o.count;
+	}
+} 
